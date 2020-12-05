5dfd2d3e8eee470d9229ecfa525b63194a3d0502
Andrew <admin@botbeetle.com>
2020-12-05 10:00:54 +0300

    Logs

---
+++ HistoryPedia/logs/FileLoggerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HistoryPedia.logs
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory, string allLogPath, string errorLogPath)
+        {
+            factory.AddProvider(new FileLoggerProvider(allLogPath, errorLogPath));
+            return factory;
+        }
+    }
+}
---
+++ HistoryPedia/logs/FileLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace HistoryPedia.logs
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string allLogPath;
+        private string errorLogPath;
+        public FileLoggerProvider(string allLogPath, string errorLogPath)
+        {
+            this.allLogPath = allLogPath;
+            this.errorLogPath = errorLogPath;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(allLogPath, errorLogPath);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
